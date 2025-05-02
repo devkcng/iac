3bb5e08110d81227ce123da2e754ab29958a9b7f
Cuong Kim <kimmanhcuong230304@gmail.com>
2025-05-02 11:23:54 +0700

    feat: add pip3 alias to profile file

--- window/ps-config-oh-my-posh/Microsoft.PowerShell_profile.ps1
+++ window/ps-config-oh-my-posh/Microsoft.PowerShell_profile.ps1
@@ -1,10 +1,12 @@
-# Initialize oh-my-posh with your custom theme
+# Initialize Oh My Posh 
 if (-not (Get-Command 'oh-my-posh' -ErrorAction SilentlyContinue)) {
     Write-Host "❌ oh-my-posh is not installed. Please install it first." -ForegroundColor Red
 } else {
-    oh-my-posh init pwsh --config ~/mytheme.omp.json | Invoke-Expression
+    $theme = "$HOME\tokyonight_storm.omp.json"
+    oh-my-posh init pwsh --config $theme | Invoke-Expression
 }
 
-# Aliases for Python
+# Python aliases
 Set-Alias python py
 Set-Alias python3 py
+Set-Alias pip3 pip
